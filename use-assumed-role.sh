5ce5e764e56bc100e78e0bd30f503bd4df018d32
bentis <ben@tisdall.org.uk>
2017-04-27 12:36:46 +0200

    p

--- use-assumed-role.sh
+++ use-assumed-role.sh
@@ -6,6 +6,5 @@ unset AWS_SECRET_ACCESS_KEY AWS_ACCESS_KEY_ID AWS_SESSION_TOKEN
 
 [ "$0" == "reset" ] && exit 0
 
-ACCOUNT_ID="$(aws sts get-caller-identity --query 'Account' --output text)"
 eval $(aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT_ID}:role/custodian-executor --role-session-name SER-1005 \
   | jq '.Credentials|["AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)","AWS_ACCESS_KEY_ID=\(.AccessKeyId)","AWS_SESSION_TOKEN=\(.SessionToken)"]|map("export \(.)")[]' -r)
